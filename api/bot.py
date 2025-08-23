2ec2900bd98823b26d046be95833e38762ff6749
^Dm <84171471+Dm-CoDeR-2021@users.noreply.github.com>
2025-08-23 15:22:50 +0330

    Update bot.py

--- api/bot.py
+++ api/bot.py
@@ -1,11 +1,54 @@
 from flask import Flask, request, jsonify
 import os
 import requests
+import random
+import json
+
+
+db = []
+last_random = 0
 
 app = Flask(__name__)
 
 TOKEN = os.environ.get("BOT_TOKEN", "")
 TELEGRAM_API = f"https://api.telegram.org/bot{TOKEN}"
+
+try:
+    with open("insults.json","r", encoding="utf-8") as f:
+        db = json.loads(f.read())
+except : {}
+
+
+def getRandomNumber(start: int, end: int) -> int:
+    r = random.randint(start, end)
+    r2 = random.randint(start, end)
+    r3 = random.randint(start, end)
+    global last_random
+
+    _r = (r * r3) / r2
+
+    while(_r > end):
+        _r = _r / random.randint(1,3)
+
+    while(_r < start):
+        _r = _r * -random.randint(1,3)
+
+    if int(_r) == last_random:
+        return int(_r) + 1 if int(_r)+1 < end else int(_r) - 1
+
+        
+    last_random = int(_r)
+    return int(_r) if _r else 0
+    
+    # for i in range(1, 100):
+    #     if i > r*3 and i % 2 == 0:
+    #         _r = random.randint(i, 100) #33
+    #         if _r % 2 != 0:
+    #             for _i in range(1, 100, 10):
+    #                 if _r > end:
+    #                     _r = _r - 10
+    #                 else:
+    #                     return _r
 
 def send_reply(chat_id, message_id, text):
     if not TOKEN:
@@ -16,6 +59,13 @@ def send_reply(chat_id, message_id, text):
         requests.post(f"{TELEGRAM_API}/sendMessage", json={"chat_id": chat_id, "text": text, "reply_to_message_id": message_id}, timeout=10)
     except Exception as e:
         print("Error sending message:", e)
+
+def send_message(chat_id, text):
+    try:
+        requests.post(f"{TELEGRAM_API}/sendMessage", json={"chat_id": chat_id, "text": text}, timeout=10)
+    except Exception as e:
+        print("Error sending message:", e)
+
 
 @app.get("/")
 def index():
@@ -37,11 +87,14 @@ def webhook():
     # elif text:
     #     send_message(chat_id, f"شما گفتید: {text}")
 
+    send_message(chat_id,text)
     reply = message.get("reply_to_message")
-
+    
     if reply:
         # بررسی اینکه ریپلای روی پیام خود ربات باشه
         if reply["from"].get("id") == 8202290017:
-            send_reply(chat_id, message["message_id"] ,"کیر میخوام")
+            for i in db:
+                if text.find(i["msg"]) != -1:
+                    send_reply(chat_id, message["message_id"], i[f"answer{getRandomNumber(1,len(i)-1)}"])
 
     return jsonify(ok=True)
