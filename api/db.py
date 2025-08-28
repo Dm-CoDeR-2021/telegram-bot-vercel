4b9583340767d835735d2727d8081db1e8999349
Dm-CoDeR-2021 <mobin.mm.u@gmail.com>
2025-08-29 00:17:07 +0330

    retert

--- api/db.py
+++ api/db.py
@@ -104,7 +104,7 @@ def Delete(table = "users", eq = "id", eq_value = 0) -> int:
 def Exist(table = "users", eq = "id", eq_value = 0) -> int:
     try:
         res = Select(table,eq,eq_value)
-        return json.dump(str(res))
+        return str(res)
     except:
         return -1
 
