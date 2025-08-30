1ab25fdec108b690294c0347e4c3815188816752
Dm-CoDeR-2021 <mobin.mm.u@gmail.com>
2025-08-30 17:35:06 +0330

    wrwer

--- api/bot.py
+++ api/bot.py
@@ -110,13 +110,13 @@ def webhook():
                             break
                     break
         try:
-            send_message(msg.chat_id, str(database.Exist(eq="id", eq_value=msg.mfrom["id"])))
-            res = database.Upsert(data={
-                "id": msg.mfrom["id"],
-                "first_name": str(msg.mfrom["first_name"]),
-                "last_name": str(msg.mfrom.get("last_name", "NULL")),
-                "username": str(msg.mfrom["username"])
-            })
+            send_message(msg.chat_id, database.Exist(eq="id", eq_value=msg.mfrom["id"]))
+            # res = database.Upsert(data={
+            #     "id": msg.mfrom["id"],
+            #     "first_name": str(msg.mfrom["first_name"]),
+            #     "last_name": str(msg.mfrom.get("last_name", "NULL")),
+            #     "username": str(msg.mfrom["username"])
+            # })
 
 
             #send_message(msg.chat_id, str(res))
