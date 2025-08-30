667653290ebd0d46c8fd8af03c9caf9778fa5e06
Dm-CoDeR-2021 <mobin.mm.u@gmail.com>
2025-08-30 17:41:20 +0330

    werwer

--- api/db.py
+++ api/db.py
@@ -104,7 +104,7 @@ def Delete(table = "users", eq = "id", eq_value = 0) -> int:
 def Exist(table = "users", eq = "id", eq_value = 0) -> int:
     try:
         res = Select(table,eq,eq_value)
-        return type(res)
+        return res.data
     except:
         return -1
 
